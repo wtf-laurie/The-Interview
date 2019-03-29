29c4c68ba39edaea7962371cb22c049e16a407bc
Laurie B <42420692+wtf-laurie@users.noreply.github.com>
2019-03-29 10:51:03 -1000

    Add files via upload

--- Admin/Check-in/like_button.js
+++ Admin/Check-in/like_button.js
@@ -1,4 +1,13 @@
 /* When the user clicks on the button,
+
+
+
+
+
+
+
+
+
 toggle between hiding and showing the dropdown content */
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
@@ -17,3 +26,27 @@ window.onclick = function(event) {
     }
   }
 }
+
+function check() {
+  document.written()
+}
+
+var $scrollable  = $(".scrollable"),
+    $scrollbar   = $(".scrollbar"),
+    height       = $scrollable.outerHeight(true),    // visible height
+    scrollHeight = $scrollable.prop("scrollHeight"), // total height
+    barHeight    = height * height / scrollHeight;   // Scrollbar height!
+
+// Scrollbar drag:
+$scrollbar.height( barHeight ).draggable({
+  axis : "y",
+  containment : "parent",
+  drag: function(e, ui) {
+    $scrollable.scrollTop( scrollHeight / height * ui.position.top  );
+  }
+});
+
+// Element scroll:
+$scrollable.on("scroll", function() {
+  $scrollbar.css({top: $scrollable.scrollTop() / height * barHeight });
+});
